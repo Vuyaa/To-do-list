d36d3796e81c9c52f9298e350b0dfab6c8999dfa
Vuyaa <st_im@hotmail.com>
2022-10-10 21:45:38 +0200

    Added missing delete item function
    And beutified using JavaScript beautifier

--- js/script.js
+++ js/script.js
@@ -1,40 +1,34 @@
+function newItem() {
+	//append classes elements and id-s to variable
+  let list = $('#list');
+	
+  let li = $('<li></li>');
 
-function newItem(){
-	
-	let list = $('#list');
-	
-	let li = $('<li></li>');
-	
-	let inputValue = $('#input').val();
-	
-	
-	li.append(inputValue);
-	
-	if(inputValue === '') {
-		alert("You must write something!");
-	}else{
-		list.append(li);
-	}
-	
-	
-	function crossOut() {
-		li.toggleClass("strike");			
-	}
-	
-	li.on("dblclick",function crossOut(){
-	li.toggleClass("strike");		
-	});
-	
-	let crossOutButton = $('<crossOutButton></crossOutButton>');
-	
-	crossOutButton.append(document.createTextNode('X'));
-	
-	li.append(crossOutButton);
-	
-	function deleteListItem(){
-		li.addClass('delete')
-		}
-	
-	list.sortable();
+  let inputValue = $('#input').val();
+
+  li.append(inputValue);
+
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    list.append(li);
+  }
+
+  function crossOut() {
+    li.toggleClass("strike");
+  }
+  li.on("dblclick", crossOut);
+
+  let crossOutButton = $('<crossOutButton></crossOutButton>');
+  crossOutButton.append(document.createTextNode('X'));
+  li.append(crossOutButton);
+
+  crossOutButton.on('click', deleteListItem);
+
+  function deleteListItem() {
+    li.addClass('delete');
+  }
+  list.sortable();
 };
+
 
