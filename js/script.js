4f279f94eb71302f508eb0b4c8c8ef01e3cd68e8
Vuyaa <st_im@hotmail.com>
2022-10-10 11:44:13 +0200

    Setting up new app :"To-do-list"
    Using jQuery, add function to add items on the list, as well as crossing items with double click

---
+++ js/script.js
@@ -0,0 +1,40 @@
+
+function newItem(){
+	
+	let list = $('#list');
+	
+	let li = $('<li></li>');
+	
+	let inputValue = $('#input').val();
+	
+	
+	li.append(inputValue);
+	
+	if(inputValue === '') {
+		alert("You must write something!");
+	}else{
+		list.append(li);
+	}
+	
+	
+	function crossOut() {
+		li.toggleClass("strike");			
+	}
+	
+	li.on("dblclick",function crossOut(){
+	li.toggleClass("strike");		
+	});
+	
+	let crossOutButton = $('<crossOutButton></crossOutButton>');
+	
+	crossOutButton.append(document.createTextNode('X'));
+	
+	li.append(crossOutButton);
+	
+	function deleteListItem(){
+		li.addClass('delete')
+		}
+	
+	list.sortable();
+};
+
